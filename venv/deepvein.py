93d59287cef391349bc21d4d5a08470bc2f43b6e
Ben Shankles <benshankles@gmail.com>
2018-08-22 22:15:02 -0500

    Added a function for "Press any key to continue" and more story

--- venv/deepvein.py
+++ venv/deepvein.py
@@ -30,6 +30,8 @@ print("""
 playerName = input("What is your name?\n")
 print("Congratulations " + playerName + " you are an alpha tester!")
 time.sleep(2)
+def clearScreen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def transmissionIncoming():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -41,6 +43,9 @@ def transmissionIncoming():
     time.sleep(2)
     print(".\n\n", end='', flush=True)
     time.sleep(2)
+
+def continueKey():
+    wait = input("Press any key to continue...")
 
 transmissionIncoming()
 
@@ -56,4 +61,46 @@ ________________________________________________________________________________
 time.sleep(1)
 print("We are looking forward to working with you!\n")
 
-wait = input("Press any key to continue.")
+continueKey()
+
+clearScreen()
+
+print("""
+Shortly after receiving this message you excitedly pack and prepare for your first voyage with the Deepvein Corporation.
+You are directed to a shipyard and shown your brand new ship! Before long, you are waving goodbye to your home planet,
+speeding towards adventure. The journey to [insert planet name] takes several months,
+even with DeepVein’s cutting edge technology; however, you spend this time in bliss,
+cryogenically frozen in your very own cryo pod. 
+
+You are finally awaken to the sound of yet another transmission. 
+""")
+
+
+continueKey()
+
+transmissionIncoming()
+
+print("DeepVein corporation: Congratulations " + playerName + "!")
+time.sleep(1)
+print("""
+______________________________________________________________________________________________________________________
+|Congratulations, """ + playerName + """ you have arrived at [insert planet placeholder name]!                        |
+|You will be delighted to be informed that you are the very first human representative to visit                       |
+|As a thank you for your dedicated time we would like you to give [insert planet placeholder name] a more proper name.|
+|Please respond with your desired choice and we will update our data systems.                                         |
+----------------------------------------------------------------------------------------------------------------------
+""")
+time.sleep(1)
+print("Thank you for working with DeepVein!\n")
+
+planetName = input("Please name the planet: ")
+
+clearScreen()
+
+print("""
+Still groggy from your slumber, you begin to feel the ship around you tremble.
+Looking out the viewscreen in the cockpit, you see that you are now breaching the deep purple outer
+atmosphere of """ + planetName + """.
+""")
+
+continueKey()
