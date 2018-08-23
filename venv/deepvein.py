7350c17ff426a5d9ebb69ccc9ef6bc92ac5a9e7a
Ben Shankles <benshankles@gmail.com>
2018-08-23 16:48:01 -0500

    Added more story.

--- venv/deepvein.py
+++ venv/deepvein.py
@@ -111,3 +111,40 @@ atmosphere of """ + planetName + """.
 """)
 
 continueKey()
+clearScreen()
+
+time.sleep(1)
+print("""
+You don your issued EVA suit and prepare your equipment for your first day on the job.
+As you wait for your ship to land and dock with the materials depository on the ground you
+hear another blip on the communications console.
+""")
+continueKey()
+
+transmissionIncoming()
+print("DeepVein Corporation: You have arrived!\n")
+time.sleep(1.5)
+print("Below are your duties for the day please report back to the ship when all are completed, thank you.\n")
+time.sleep(1)
+print("""
+ ____________________________________________________________________________________________________________________
+|-Access the depository and transit lines.                                                                           | 
+|-Visit both mining operations at the mountains to the east and west.                                                |
+|-Confirm that all solar panels are operating efficiently and that there are no breaks in the power supply.          |
+ --------------------------------------------------------------------------------------------------------------------
+""")
+continueKey()
+clearScreen()
+time.sleep(1)
+
+print("""
+The ship gives one last shudder as it finishes its docking process and the lights in the cockpit shift
+to a soft green hue, signifying that it is safe to disembark.
+You enter the airlock and cycle it, depressurizing the ship and revealing the outside world.
+Directly in front of you lies the massive depository building where the processed materials are stored and prepared
+for transit back to Deepvien Headquarters. Running east and west from the building are two roads beaten into the dust
+by large transit vehicles, bristling with solar panels and sensor arrays. You see a couple of these in the distance
+heading towards the depository. Beyond the depository lies the solar field where hundreds more solar panels lie
+in the green-gray dust. The day is yours what duty would you like to accomplish first? 
+""")
+
